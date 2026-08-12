c7810b361d8530d4e9a29b9d9d492d3b0421a2cb
yonixxxxx <yonixxxxx@example.com>
2026-08-13 01:32:59 +0300

    Initial commit

--- app/db/config.py
+++ app/db/config.py
@@ -1,27 +1,3 @@
-# from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-# from fastapi import Depends
-# from typing import AsyncGenerator, Annotated
-# from decouple import config
-#
-# DB_USER = config("DB_USER")
-# DB_PASS = config("DB_PASS")
-# DB_NAME = config("DB_NAME")
-# DB_PORT = config("DB_PORT", cast=int)
-# DB_HOST = config("DB_HOST")
-#
-# DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-#
-#
-# engine = create_async_engine(DATABASE_URL,echo=True,future=True)
-#
-# async_session = async_sessionmaker(bind=engine, expire_on_commit=False,class_=AsyncSession)
-#
-# async def get_session() -> AsyncGenerator[AsyncSession, None]:
-#     async with async_session() as session:
-#         yield session
-#
-# SessionDep = Annotated[AsyncSession, Depends(get_session)]
-
 from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
